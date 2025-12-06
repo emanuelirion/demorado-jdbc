2cb9e3bdffd618be45f44f7ad10e9dc113d95a4d
Emanuel <2310100375>
2025-12-06 13:36:03 -0300

    Reusing instantiation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -9,6 +9,8 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
     SellerDao sellerDao = DaoFactory.createSellerDao();
+    
     Seller seller = sellerDao.findById(3);
+
     System.out.println(seller);}
 }

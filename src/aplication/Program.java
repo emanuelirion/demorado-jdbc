83d5bc88bfe6633ba2ff60d8be9e485e2708e553
Emanuel <2310100375>
2025-12-06 12:16:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,5 +1,8 @@
 package aplication;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,5 +12,6 @@ public class Program {
     public static void main(String[] args) {
     Department obj = new Department(1,"Books");
     Seller seller = new Seller(21,"Bob", "bog@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
     System.out.println(seller);}
 }
